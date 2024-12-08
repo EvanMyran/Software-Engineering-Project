e41b01810ce917ba488b88726f1943af8663169c
Evan Myran <evankmyran@gmail.com>
2024-12-08 12:34:23 -0600

    Update checkoutButton.js new stingify statement

--- src/components/checkoutButton.js
+++ src/components/checkoutButton.js
@@ -9,7 +9,7 @@ const CheckoutButton = ({ cart, isCardValid, isCodeValid, isDateValid, selectedP
       const response = await fetch('http://localhost:3000/api/checkout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ tickets: cart, userId }),
+        body: JSON.stringify({ tickets: cart, userId: sessionStorage.getItem('userId') }),
       });
 
       if (!response.ok) {
